a566919ac15d853ec5ddbb6a9e277c4eef6637dc
Tomas Sychra <sychrat@gmail.com>
2024-02-03 22:51:37 +0100

    Typography

--- packages/components/Typography/hooks/useCreateTypographyStyles.ts
+++ packages/components/Typography/hooks/useCreateTypographyStyles.ts
@@ -3,59 +3,27 @@ import { Theme, TypographyStylesProps } from 'types';
 export const useCreateTypographyStyles = (theme: Theme, stylesProps: TypographyStylesProps) => {
   const { variant, color, truncate } = stylesProps;
 
-  const getRootColor = () => {
-    switch (color) {
-      case 'primary':
-        return theme.palette.primary.main;
-
-      case 'secondary':
-        return theme.palette.secondary.main;
-
-      case 'tertiary':
-        return theme.palette.tertiary.main;
-
-      case 'error':
-        return theme.palette.error.main;
-
-      case 'warning':
-        return theme.palette.warning.main;
-
-      case 'info':
-        return theme.palette.info.main;
-
-      case 'success':
-        return theme.palette.success.main;
-
-      case 'inverted':
-        return theme.palette.inverted.main;
-
-      case 'text-primary':
-        return theme.palette.text.primary;
-
-      case 'text-secondary':
-        return theme.palette.text.secondary;
-
-      case 'text-tertiary':
-        return theme.palette.text.tertiary;
-
-      case 'text-muted':
-        return theme.palette.text.muted;
-
-      case 'text-disabled':
-        return theme.palette.text.disabled;
-
-      case 'inherit':
-      default:
-        return 'inherit';
-    }
+  const rootColorMap = {
+    primary: theme.palette.primary.main,
+    secondary: theme.palette.secondary.main,
+    tertiary: theme.palette.tertiary.main,
+    error: theme.palette.error.main,
+    warning: theme.palette.warning.main,
+    info: theme.palette.info.main,
+    success: theme.palette.success.main,
+    inverted: theme.palette.inverted.main,
+    'text-primary': theme.palette.text.primary,
+    'text-secondary': theme.palette.text.secondary,
+    'text-tertiary': theme.palette.text.tertiary,
+    'text-muted': theme.palette.text.muted,
+    'text-disabled': theme.palette.text.disabled,
+    inherit: 'inherit',
   };
 
   const rootBase = {
-    color: getRootColor(),
+    color: rootColorMap[color],
   };
-
   const rootVariant = theme.typography[variant];
-
   const rootTruncate = truncate
     ? {
         whiteSpace: 'nowrap',
